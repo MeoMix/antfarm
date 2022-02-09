1d4be6c3368193cce090a290ca424730ed8a6817
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2022-02-08 20:53:57 -0800

    Cut unused values

--- src/createAnt.ts
+++ src/createAnt.ts
@@ -1,6 +1,6 @@
 import type { Direction } from "./types";
 
-export type Behavior = 'wandering' | 'carrying' | 'panic';
+export type Behavior = 'wandering' | 'carrying';
 
 export type Ant = {
   x: number;
@@ -9,18 +9,16 @@ export type Ant = {
   facingDirection: Direction;
   footDirection: Direction;
   timer: number;
-  phase: number;
 }
 
 const BehaviorTimingFactors = {
   wandering: 4,
   carrying:  5,
-  panic: 1,
 }
 
 export const getTimer = (behavior: Behavior) => BehaviorTimingFactors[behavior] + Math.floor((Math.random() * 3)) - 1;
 
-function createAnt(x = 0, y = 0, behavior = 'wandering' as const, facingDirection: Direction, footDirection: Direction): Ant {
+function createAnt(x: number, y: number, behavior: Behavior, facingDirection: Direction, footDirection: Direction): Ant {
   return {
     x,
     y,
@@ -28,7 +26,6 @@ function createAnt(x = 0, y = 0, behavior = 'wandering' as const, facingDirectio
     facingDirection,
     footDirection,
     timer: getTimer(behavior),
-    phase: 0,
   };
 }
 
