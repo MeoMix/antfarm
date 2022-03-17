8cf43dc8d188b0a87b3984cecf17525aae6d6f3d
Sean Anderson <sanderson@foreground.co>
2022-03-16 20:27:50 -0700

    refactor: use angle-based orientation not foot/facing directions

--- src/createAnt.ts
+++ src/createAnt.ts
@@ -1,13 +1,26 @@
-import type { Direction } from "./types";
-
 export type Behavior = 'wandering' | 'carrying';
+export type Facing = 'left' | 'right';
+
+export const angles = [0, 90, 180, 270] as const;
+export type Angle = typeof angles[number];
+
+/**
+ * Rotation is a value from 0 to 3. A value of 1 is a 90 degree counter-clockwise rotation. Negative values are accepted.
+ * Examples:
+ *  getRotatedAngle(0, -1); // 270
+ *  getRotatedAngle(0, 1); // 90
+ */
+export const getRotatedAngle = (angle: Angle, rotation: number): Angle => {
+  const rotatedIndex = (angles.indexOf(angle) - rotation) % angles.length;
+  return angles[rotatedIndex < 0 ? angles.length + rotatedIndex : rotatedIndex];
+};
 
 export type Ant = {
   x: number;
   y: number;
   behavior: Behavior;
-  facingDirection: Direction;
-  footDirection: Direction;
+  facing: Facing,
+  angle: Angle,
   timer: number;
 }
 
@@ -18,13 +31,13 @@ const BehaviorTimingFactors = {
 
 export const getTimer = (behavior: Behavior) => BehaviorTimingFactors[behavior] + Math.floor((Math.random() * 3)) - 1;
 
-function createAnt(x: number, y: number, behavior: Behavior, facingDirection: Direction, footDirection: Direction): Ant {
+function createAnt(x: number, y: number, behavior: Behavior, facing: Facing, angle: Angle): Ant {
   return {
     x,
     y,
     behavior,
-    facingDirection,
-    footDirection,
+    facing,
+    angle,
     timer: getTimer(behavior),
   };
 }
